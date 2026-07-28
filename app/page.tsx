882f40274346944491dc4dad52013f1181c55562
Aaron Villalobos <aaron.villalobos4@gmail.com>
2026-07-27 22:47:53 -0500

    Update logo src extension to match uppercase logo.PNG

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         {/* Header / Brand Logo */}
         <div className="flex flex-col items-center justify-center gap-3">
           <Image
-            src="/logo.PNG"
+            src="/logo.png"
             alt="Game Time Logo"
             width={120}
             height={120}
